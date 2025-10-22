d76d3a343eee07ffec5e11da0c87b47a50fe4c2f
mhlsjk <mhlsjk@gmail.com>
2025-10-22 23:39:58 +0300

    Added finishing touches

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine.InputSystem;
 
 public class GameBoard : MonoBehaviour {
@@ -16,11 +17,25 @@ public class GameBoard : MonoBehaviour {
   [SerializeField] private int random_field_height = 30;
   [SerializeField, Range(0f, 1f)] private float random_field_density = 0.2f;
   [SerializeField] public bool in_edit_mode = false;
+  
+  [SerializeField] private bool pvp_mode = false;
+  [SerializeField] public int starting_pieces_each = 10;
+  private bool pvp_setup_active = false;
+  private int current_placing_player = 1;
+  private int placed_count_current_player = 0;
+
+  private int player1_score = 0;
+  private int player2_score = 0;
 
   private HashSet<Vector3Int> alive_cells;
   private HashSet<Vector3Int> cells_to_check;
 
   private Coroutine sim_coroutine;
+
+  private int iterations_count = 0;
+  [SerializeField] public int max_iterations = -1;
+
+  [SerializeField] private TMP_Text announcer_text;
 
   private void Awake() {
     alive_cells = new HashSet<Vector3Int>();
@@ -34,16 +49,56 @@ public class GameBoard : MonoBehaviour {
   }
 
   private void Update() {
-    if (in_edit_mode && Mouse.current.leftButton.wasPressedThisFrame) {
+    if (in_edit_mode && pvp_setup_active) {
+      if (Mouse.current.leftButton.wasPressedThisFrame) {
+        Vector3 world_point = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Vector3Int cell = current_state.WorldToCell(world_point);
+        bool is_white = (current_placing_player == 1);
+        if (current_state.GetTile(cell) == white_tile && is_white || 
+            current_state.GetTile(cell) == red_tile && !is_white) {
+          return;
+        }
+          
+        SetAlive(cell, true, is_white);
+
+        placed_count_current_player++;
+        if (placed_count_current_player >= starting_pieces_each) {
+          if (current_placing_player == 1) {
+            current_placing_player = 2;
+            placed_count_current_player = 0;
+            announcer_text.SetText("Player 2 (red) place your pieces.");
+          } else {
+            FinishPvPSetup();
+          }
+        }
+      }
+
+      if (Mouse.current.rightButton.wasPressedThisFrame) {
+        Vector3 world_point = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Vector3Int cell = current_state.WorldToCell(world_point);
+        bool is_white = (current_state.GetTile(cell) == white_tile);
+        if (is_white && current_state.GetTile(cell) != white_tile ||
+            !is_white && current_state.GetTile(cell) != red_tile) {
+          return;
+        }
+        
+        SetAlive(cell, false, true);
+        placed_count_current_player--;
+      }
+
+      return;
+    }
+    
+    if (!pvp_mode && in_edit_mode && Mouse.current.leftButton.wasPressedThisFrame) {
       Vector3 world_point = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
       Vector3Int cell = current_state.WorldToCell(world_point);
       ToggleCell(cell, true);
     }
 
-    if (in_edit_mode && Mouse.current.rightButton.wasPressedThisFrame) {
+    if (!pvp_mode && in_edit_mode && Mouse.current.rightButton.wasPressedThisFrame) {
       Vector3 world_point = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
       Vector3Int cell = current_state.WorldToCell(world_point);
-      ToggleCell(cell, false);
+      ToggleCell(cell, true);
     }
   }
 
@@ -57,6 +112,8 @@ public class GameBoard : MonoBehaviour {
       current_state.SetTile(cell, white_tile);
       alive_cells.Add(cell);
     }
+
+    iterations_count = 0;
   }
 
   public void Clear() {
@@ -127,9 +184,44 @@ public class GameBoard : MonoBehaviour {
     yield return interval;
     while (enabled) {
       UpdateState();
+      iterations_count += 1;
       interval = new WaitForSeconds(update_interval);
       yield return interval;
     }
+  }
+
+  public void StartPvPSetup(int piecesPerPlayer) {
+    Clear();
+    pvp_mode = true;
+    pvp_setup_active = true;
+    current_placing_player = 1;
+    placed_count_current_player = 0;
+    player1_score = 0;
+    player2_score = 0;
+    announcer_text.SetText("Player 1 (white) place your pieces.");
+  }
+
+  private void FinishPvPSetup() {
+    pvp_setup_active = false;
+    player1_score = 0;
+    player2_score = 0;
+    announcer_text.SetText("PVP setup Complete! Resume the game.");
+  }
+
+  public void EndGameManual() {
+    EndGame();
+  }
+
+  private void EndGame() {
+    PauseSimulation();
+    pvp_setup_active = false;
+    pvp_mode = false;
+    string result;
+    if (player1_score > player2_score) result = $"Player 1 (white) wins {player1_score} : {player2_score}";
+    else if (player2_score > player1_score) result = $"Player 2 (red) wins {player2_score} : {player1_score}";
+    else result = $"Draw {player1_score} : {player2_score}";
+    announcer_text.SetText("Game Over! " + result);
+    StartSimulation();
   }
 
   private void UpdateState() {
@@ -163,8 +255,13 @@ public class GameBoard : MonoBehaviour {
         }
         else {
           if (total_neighbors == 3) {
-            next_state.SetTile(cell, soleIsWhite ? white_tile : red_tile);
+            bool bornWhite = soleIsWhite;
+            next_state.SetTile(cell, bornWhite ? white_tile : red_tile);
             new_alive.Add(cell);
+            if (pvp_mode) {
+              if (bornWhite) player1_score++;
+              else player2_score++;
+            }
           }
         }
       }
@@ -186,14 +283,16 @@ public class GameBoard : MonoBehaviour {
         }
         else {
           if (total_neighbors == 3) {
-            if (alive_neighbors_white > alive_neighbors_red) {
-              next_state.SetTile(cell, white_tile);
-            }
-            else {
-              next_state.SetTile(cell, red_tile);
-            }
+            bool bornWhite = alive_neighbors_white > alive_neighbors_red;
+            if (bornWhite) next_state.SetTile(cell, white_tile);
+            else next_state.SetTile(cell, red_tile);
 
             new_alive.Add(cell);
+
+            if (pvp_mode) {
+              if (bornWhite) player1_score++;
+              else player2_score++;
+            }
           }
         }
       }
@@ -205,6 +304,10 @@ public class GameBoard : MonoBehaviour {
     next_state.ClearAllTiles();
 
     alive_cells = new_alive;
+    
+    if (pvp_mode && alive_cells.Count == 0 || (max_iterations >= 0 && iterations_count >= max_iterations)) {
+      EndGame();
+    }
   }
 
   private (int, int) CountAliveNeighbors(Vector3Int cell) {
