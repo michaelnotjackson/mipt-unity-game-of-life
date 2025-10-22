97f558775ab146f1de2184deca287d0e1e6e4503
mhlsjk <mhlsjk@gmail.com>
2025-10-22 21:03:53 +0300

    Added pause menu and 2 player

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -7,20 +7,20 @@ using UnityEngine.InputSystem;
 public class GameBoard : MonoBehaviour {
   [SerializeField] private Tilemap current_state;
   [SerializeField] private Tilemap next_state;
-  [SerializeField] private Tile alive_tile;
-  [SerializeField] private Tile dead_tile;
+  [SerializeField] private Tile white_tile;
+  [SerializeField] private Tile red_tile;
   [SerializeField] private Pattern pattern;
   [SerializeField] private float update_interval = 0.05f;
 
   [SerializeField] private int random_field_width = 40;
   [SerializeField] private int random_field_height = 30;
   [SerializeField, Range(0f, 1f)] private float random_field_density = 0.2f;
+  [SerializeField] public bool in_edit_mode = false;
 
   private HashSet<Vector3Int> alive_cells;
   private HashSet<Vector3Int> cells_to_check;
 
   private Coroutine sim_coroutine;
-  [SerializeField] private bool is_running = false;
 
   private void Awake() {
     alive_cells = new HashSet<Vector3Int>();
@@ -34,15 +34,16 @@ public class GameBoard : MonoBehaviour {
   }
 
   private void Update() {
-    if (!is_running && Mouse.current.leftButton.wasPressedThisFrame) {
+    if (in_edit_mode && Mouse.current.leftButton.wasPressedThisFrame) {
       Vector3 world_point = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
       Vector3Int cell = current_state.WorldToCell(world_point);
-      ToggleCell(cell);
+      ToggleCell(cell, true);
     }
     
-    if (Keyboard.current.spaceKey.wasPressedThisFrame) {
-      TogglePlayPause();
-      Debug.Log("space pressed, is_running: " + is_running);
+    if (in_edit_mode && Mouse.current.rightButton.wasPressedThisFrame) {
+      Vector3 world_point = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+      Vector3Int cell = current_state.WorldToCell(world_point);
+      ToggleCell(cell, false);
     }
   }
 
@@ -53,18 +54,18 @@ public class GameBoard : MonoBehaviour {
 
     for (int i = 0; i < pattern.cells.Length; i++) {
       Vector3Int cell = (Vector3Int)(pattern.cells[i] - center);
-      current_state.SetTile(cell, alive_tile);
+      current_state.SetTile(cell, white_tile);
       alive_cells.Add(cell);
     }
   }
 
-  private void Clear() {
+  public void Clear() {
     current_state.ClearAllTiles();
     next_state.ClearAllTiles();
   }
 
   public void TogglePlayPause() {
-    if (is_running) {
+    if (!PauseMenu.is_paused) {
       PauseSimulation();
     }
     else {
@@ -73,16 +74,16 @@ public class GameBoard : MonoBehaviour {
   }
 
   public void StartSimulation() {
-    if (is_running) {
+    if (!PauseMenu.is_paused) {
       return;
     }
-    
+
     sim_coroutine = StartCoroutine(Simulate());
-    is_running = true;
+    PauseMenu.is_paused = false;
   }
-  
+
   public void PauseSimulation() {
-    if (!is_running) {
+    if (PauseMenu.is_paused) {
       return;
     }
 
@@ -90,27 +91,31 @@ public class GameBoard : MonoBehaviour {
       StopCoroutine(sim_coroutine);
     }
     sim_coroutine = null;
-    is_running = false;
+    PauseMenu.is_paused = true;
   }
 
   public void SetSpeed(float update_interval) {
     this.update_interval = update_interval;
-    if (is_running) {
+    if (!PauseMenu.is_paused) {
       PauseSimulation();
       StartSimulation();
     }
   }
+  
+  public float GetUpdateInterval() {
+    return update_interval;
+  }
 
   public void Randomize() {
     Clear();
-    
+
     Vector2Int center = new Vector2Int(random_field_width, random_field_height) / 2;
 
     for (int x = -center.x; x <= center.x; x++) {
       for (int y = -center.y; y <= center.y; y++) {
         if (Random.value <= random_field_density) {
           Vector3Int cell = new Vector3Int(x, y, 0);
-          SetAlive(cell, true);
+          SetAlive(cell, true, Random.Range(0, 1) == 1);
         }
       }
     }
@@ -138,26 +143,32 @@ public class GameBoard : MonoBehaviour {
     }
 
     foreach (Vector3Int cell in cells_to_check) {
-      int alive_neighbors = CountAliveNeighbors(cell);
+      (int alive_neighbors_white, int alive_neighbors_red) = CountAliveNeighbors(cell);
       bool currently_alive = IsAlive(cell);
+      bool is_white = current_state.GetTile(cell) == white_tile;
 
-      if (currently_alive) {
-        if (alive_neighbors < 2 || alive_neighbors > 3) {
-          next_state.SetTile(cell, dead_tile);
-          alive_cells.Remove(cell);
-        }
-        else {
-          next_state.SetTile(cell, alive_tile);
-        }
+      if (is_white && alive_neighbors_white < 2) {
+        next_state.SetTile(cell, null);
+        currently_alive = false;
       }
-      else {
-        if (alive_neighbors == 3) {
-          next_state.SetTile(cell, alive_tile);
-          alive_cells.Add(cell);
+      if (!is_white && alive_neighbors_red < 2) {
+        next_state.SetTile(cell, null);
+        currently_alive = false;
+      }
+      
+      if (currently_alive) {
+        if (alive_neighbors_white + alive_neighbors_red >= 3) {
+          if (alive_neighbors_white > alive_neighbors_red) {
+            next_state.SetTile(cell, white_tile);
+          } else {
+            next_state.SetTile(cell, red_tile);
+          }
+          continue;
         }
-        else {
-          next_state.SetTile(cell, dead_tile);
+        if (alive_neighbors_white + alive_neighbors_red < 2) {
+          next_state.SetTile(cell, null);
         }
+        continue;
       }
     }
 
@@ -167,43 +178,53 @@ public class GameBoard : MonoBehaviour {
     next_state.ClearAllTiles();
   }
 
-  private int CountAliveNeighbors(Vector3Int cell) {
-    int count = 0;
+  private (int, int) CountAliveNeighbors(Vector3Int cell) {
+    int count_white = 0;
+    int count_red = 0;
     for (int x = -1; x <= 1; x++) {
       for (int y = -1; y <= 1; y++) {
         if (x == 0 && y == 0) continue;
 
         Vector3Int neighbor = cell + new Vector3Int(x, y, 0);
         if (IsAlive(neighbor)) {
-          count++;
+          if (current_state.GetTile(neighbor) == white_tile) {
+            count_white++;
+          } else {
+            count_red++;
+          }
         }
       }
     }
 
-    return count;
+    return (count_white, count_red);
   }
 
   private bool IsAlive(Vector3Int cell) {
-    return current_state.GetTile(cell) == alive_tile;
+    return current_state.HasTile(cell);
   }
 
-  public void SetAlive(Vector3Int cell, bool alive) {
+  public void SetAlive(Vector3Int cell, bool alive, bool is_white) {
     if (alive) {
-      current_state.SetTile(cell, alive_tile);
+      if (is_white) {
+        current_state.SetTile(cell, white_tile);
+      }
+      else {
+        current_state.SetTile(cell, red_tile); 
+      }
       alive_cells.Add(cell);
     }
     else {
-      current_state.SetTile(cell, dead_tile);
+      current_state.SetTile(cell, null);
       alive_cells.Remove(cell);
     }
   }
-  
-  private void ToggleCell(Vector3Int cell) {
+
+  private void ToggleCell(Vector3Int cell, bool is_white) {
     if (IsAlive(cell)) {
-      SetAlive(cell, false);
+      SetAlive(cell, false, is_white);
     }
     else {
-      SetAlive(cell, true);
+      SetAlive(cell, true, is_white);
     }
   }
 }
